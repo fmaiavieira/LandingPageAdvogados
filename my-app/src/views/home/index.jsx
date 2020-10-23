cb829601370309cb292afa2ca53dde13a02a2b1d
Vanderson Sander <vanderson_sander@hotmail.com>
2020-10-23 16:57:40 -0300

    Juntado Header

--- my-app/src/views/home/index.jsx
+++ my-app/src/views/home/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Banner from '../../components/Banner'
 import Contato from '../../components/Contato'
 
 const Home = () => {
     return (
         <>
+            <Header />
+            <Banner />
             <Contato />
         </>
     )
