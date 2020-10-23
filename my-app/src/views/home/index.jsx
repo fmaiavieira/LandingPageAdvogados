2bbb9276752f9453a5f0b2bd05158c4766e427ab
Vanderson Sander <vanderson_sander@hotmail.com>
2020-10-23 17:05:26 -0300

    Juntado footer

--- my-app/src/views/home/index.jsx
+++ my-app/src/views/home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import Contato from '../../components/Contato'
+import Footer from '../../components/footer'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Header />
             <Banner />
             <Contato />
+            <Footer />
         </>
     )
 }
